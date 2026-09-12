eec2467d40be27ce24d4cd85baff59bb597c426d
Sara Garcia <abigailfigueroafigueroa56@gmail.com>
2026-09-12 16:28:23 -0600

    MascotaVirtual

--- MascotaVirtual.java
+++ MascotaVirtual.java
@@ -2,8 +2,47 @@ class MascotaVirtual{
     public String color;
     private Integer nombre;
     public String misiones;
-    public String comer() {
+    public String comer();
+    public String jugar() ;
+    public Integer energia;
+
+    public MascotaVirtual(String nombreElegido){
+
+        nombre=nombreElegido;
+    energia=100;
+
+    System.out.print ("Bienvenido al mundo");
+    System.out.println(nombreElegido);
+
     }
-    public String jugar() {
-    }
+    
+
+    public void saltar(){
+        System.out.printl(x:"la mascota esta saltando");
+        energia= energia - 5;
+
+        System.out.print (x:"El nivel de energia bajo a horas");
+        System.out.println(energia);
+
 }
+
+ public void saltar(){
+     System.out.printl(x:"la mascota esta corriendo");
+     energia= energia - 9;
+     System.out.print (x:"El nivel de energia bajo a horas");
+     System.out.println(energia);
+
+ }
+
+  public void saltar(){
+     System.out.printl(x:"la mascota esta durmiendo");
+     energia= energia + 10;
+     System.out.print (x:"El nivel de energia bajo a horas");
+     System.out.println(energia);
+
+  }
+
+}
+    
+
+
