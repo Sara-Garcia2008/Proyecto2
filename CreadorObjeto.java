e61b8ed93c12b4523d26dfb762d7f06de9a47887
Sara Garcia <abigailfigueroafigueroa56@gmail.com>
2026-09-19 14:38:07 -0600

    Creador

--- CreadorObjeto.java
+++ CreadorObjeto.java
@@ -1,5 +1,10 @@
 class CreadorObjeto {
-    public static void main(String[] args) {
-   
-    }
+    public static void main(String() args) {
+        MascotaVirtual objeto1= new MascotaVirtual("Nombre Elegido:Tify");
+        MascotaVirtual objeto2= new MascotaVirtual("Nombre Elegido:Sify");
+       
+     
+      }
 }
+   
+    
