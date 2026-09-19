e2424950f7553385d78b576d1ce975aec5c47e0d
Sara Garcia <abigailfigueroafigueroa56@gmail.com>
2026-09-19 14:27:48 -0600

    colores

---
+++ Colores.java
@@ -0,0 +1,6 @@
+public class colores {
+public static final String VERDE="/u001B[32m";
+public static final String ROJO="/u001B[31m";
+public static final String AMARILLO="/u00b1[33m";
+public static final String RESET="/u001B[0m";
+}
