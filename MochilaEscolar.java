a5bdcbcb5ce62a998b4dd094b91bfcbee7200210
Sara Garcia <abigailfigueroafigueroa56@gmail.com>
2026-09-19 14:32:02 -0600

    mochila

---
+++ MochilaEscolar.java
@@ -0,0 +1,15 @@
+public class MochilaEscolar{
+    public String color;
+    public String Marca;
+    public Doble tamaño;
+    public Integer capacidad;
+
+    public MochilaEscolar(String nombredelaclase){
+        
+    }
+
+
+
+
+
+}
